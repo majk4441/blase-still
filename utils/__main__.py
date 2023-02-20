f25794341e23ff3de81bddda9b2b32ac962b96a6
jrd <119338995+majk4441@users.noreply.github.com>
2023-02-20 20:14:11 +0100

    Update __main__.py

--- utils/__main__.py
+++ utils/__main__.py
@@ -1,8 +1,6 @@
 # no, u won't get blase source.
-# blase is closed-source project, even if it has a few commands and i won't change it.
-
-# no, u won't get blase source.
 # blase is closed-source project.
+
 
             No skidding?
 ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
